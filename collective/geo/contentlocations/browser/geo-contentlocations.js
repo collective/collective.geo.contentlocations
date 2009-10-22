f81c4a047c13d7f2b25a67d92eab4237c1878a2d
Gerhard Weis <gerhard@weis.au.eu.org>
2009-10-22 01:33:57 +0000

    * added geo-contentlocations.js
    git-svn-id: http://svn.plone.org/svn/collective/collective.geo.contentlocations/branches/geosettings-macros@100092 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/geo/contentlocations/browser/geo-contentlocations.js
@@ -0,0 +1,51 @@
+
+
+jq(document).ready(function() {
+                     console.log("Add Edit Layer");
+  var map = cgmap.config.geoshapemap.map;
+  var editlayer = new OpenLayers.Layer.Vector("Edit");
+  map.addLayer(editlayer);
+
+  function onEditBeforeFeaturesAdded(evt)
+  {
+    // TODO: maybe check editingpanel.multi (custom attribute) allowing
+    // multiple shapes at once. evt object should hold a reference to control
+    // to check this attribute (or I need to add this attribute to the
+    // editinglayer?
+    evt.object.destroyFeatures();
+  }
+
+  editlayer.events.register("beforefeaturesadded", editlayer,
+                            onEditBeforeFeaturesAdded);
+
+  var editingpanel = new OpenLayers.Control.EditingToolbar(editlayer, {});
+  editingpanel.addControls([ new OpenLayers.Control.ModifyFeature(editlayer, {}) ]);
+
+  map.addControl(editingpanel);
+  editingpanel.activate();
+
+  function olupdateWidget(evt)
+  {
+    var out_options = {
+      internalProjection: evt.object.map.projection,
+      externalProjection: evt.object.map.displayProjection };
+    var format = new OpenLayers.Format.WKT(out_options);
+    document.getElementById(cgmap.wkt_widget_id).value = format.write(evt.feature);
+    format.destroy();
+  }
+
+  editlayer.events.register("featureadded", editlayer, olupdateWidget);
+  editlayer.events.register("featuremodified", editlayer, olupdateWidget);
+
+  var geomwkt = document.getElementById(cgmap.wkt_widget_id).value;
+  var in_options = {
+    internalProjection: map.projection,
+    externalProjection: map.displayProjection };
+  var format = new OpenLayers.Format.WKT(in_options);
+  var feat = format.read(geomwkt);
+  if (feat)
+  {
+    editlayer.addFeatures([feat]);
+  }
+
+});
