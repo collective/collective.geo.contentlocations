927013f23a4022eae8559f6d8ca5dfbb1ac5cf57
Giorgio Borelli <giorgio@giorgioborelli.it>
2010-05-03 16:55:50 +0000

    fixed tests
    git-svn-id: http://svn.plone.org/svn/collective/collective.geo.contentlocations/trunk@116780 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/geo/contentlocations/tests/base.py
+++ collective/geo/contentlocations/tests/base.py
@@ -1,10 +1,15 @@
 """Test setup for integration and functional tests."""
+from zope.component import provideAdapter
+from Products.CMFCore.PortalContent import PortalContent
 
 from Products.Five import zcml
 from Products.Five import fiveconfigure
 
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import onsetup
+
+from collective.geo.settings.interfaces import IGeoCustomFeatureStyle
+from collective.geo.contentlocations.geostylemanager import GeoStyleManager
 
 
 @onsetup
@@ -28,4 +33,14 @@ class TestCase(ptc.PloneTestCase):
 
 
 class FunctionalTestCase(ptc.FunctionalTestCase):
-    pass
+
+    def afterSetUp(self):
+        super(FunctionalTestCase, self).afterSetUp()
+        # register adapter for custom styles
+        provideAdapter(GeoStyleManager, (PortalContent,), IGeoCustomFeatureStyle)
+
+    def tearDown(self):
+        # unregister IGeoCustomFeatureStyle adapter
+        from zope.component import getGlobalSiteManager
+        gsm = getGlobalSiteManager()
+        gsm.unregisterAdapter(GeoStyleManager, (PortalContent,), IGeoCustomFeatureStyle) 
